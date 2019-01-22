8e72a5b9b7b8d2c4e4490a588850815dec3e17e6
nik849 <nickhale@protonmail.ch>
2019-01-22 10:29:47 +0000

    Started to add stack capability

--- Python/tifftoniff/cli.py
+++ Python/tifftoniff/cli.py
@@ -7,13 +7,18 @@ import os
 @click.option(
     '--output', '-o', default='_output',
     help='Ouput folder name/path')
+@click.option(
+    '--stack', '-s', default=None,
+    help='Path to tif stack to convert')
 
-def run(input, output):
+def run(input, output, stack):
     if not os.path.exists("./" + output):
         os.makedirs("./" + output)
-
-    tiff = tifftoniff.Convert(input, output)
-    tiff.convert()
+    if stack is not None:
+        pass
+    else:
+        tiff = tifftoniff.Convert(input, output)
+        tiff.convert()
 
 if __name__ == '__main__':
     run()
